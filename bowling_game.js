f34b7ca81b47439c9316e85546010c9626108e41
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2018-07-31 01:08:49 +0000

    End of 90 min

---
+++ bowling_game.js
@@ -0,0 +1,15 @@
+function bowlingscore(arr) {
+    
+    
+    const num1 = [1, 4];
+    const num2 = []
+    const reducer = (accumulator, currentValue) => accumulator + currentValue;
+    
+
+    return num1;  
+   
+
+
+
+
+console.log(num1.reduce(reducer));
